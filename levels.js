4f808f888ab9e2da2221bed4d0ef2cacb9377652
Rahul Vij <rahulvij@Rahuls-MacBook-Pro.local>
2022-08-22 15:07:52 +0530

    Final Commit

--- levels.js
+++
@@ -1,5 +0,0 @@
-const levels = [
-  "JavaScript is arguably more difficult to learn than related technologies such as HTML and CSS. Before attempting to learn JavaScript, you are strongly advised to get familiar with at least these two technologies first, and perhaps others as well.",
-  "We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.",
-  "The features we teach in this course are stable and have been supported by all major browsers for several years. You're only likely to have trouble using them in obsolete browsers, in particular Internet Explorer.",
-];
